96030c0c056f774b53eb3983b6163df32fbfa361
kaurluhaaar <kaur.luhaaar@ametikool.ee>
2023-05-22 14:52:34 +0300

    adjustments

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -14,7 +14,7 @@ function Company({ html, company }) {
 
     return (
         <>
-            <div className="w-fit mx-10 h-screen flex flex-col justify-center overflow-clip">
+            <div className="w-fit px-10 h-screen flex flex-col justify-center overflow-clip">
                 <div>
                     <div className='flex items-end'>
                         <a href={`https://${company.domain}`} className="flex">
@@ -28,17 +28,17 @@ function Company({ html, company }) {
                     </div>
                     <div className='h-[2px] mb-5 mt-5 w-300 -rotate-180 bg-gradient-to-r from-space-blue via-text-blue-dark to-morning-blue'></div>
                 </div>
-                <div className="flex mt-10">
+                <div className="flex mt-10 ">
                     <div className="mr-12 w-1/2">
                         <div className=" flex gap-4 rounded-lg ml-8">
                             <div className="h-8 font-nunito font-normal bg-button-blue text-morning-blue flex pl-2 pr-2 p-1 rounded-lg w-max">Industry<span className="pl-4 text-text-blue">{company.industryMain}</span></div>
                             <div className="h-8 font-nunito font-normal bg-button-blue text-morning-blue flex pl-2 pr-2 p-1 rounded-lg w-max">Founded <span className="pl-4 text-text-blue">{company.founded}</span></div>
                         </div>
                         <div className="flex my-6">
-                            <div className="text-text-blue h-80 mt-5 px-8 mb-16 overflow-y-auto">{company.description}</div>
+                            <div className="text-text-blue h-80 mt-5 pr-16 mb-16 overflow-y-auto ">{company.description}</div>
                             <div className="w-4 h-100 bg-gradient-to-b from-morning-blue to-space-blue"></div>
                         </div>
-                        <div className="flex gap-3 pr-8 justify-end">
+                        <div className="flex gap-3 pr-16 justify-end">
                             <a href={`https://${company.facebook}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill='#5856B5' viewBox="0 0 50 50" width="40px" height="40px"><path d="M 9 4 C 6.2504839 4 4 6.2504839 4 9 L 4 41 C 4 43.749516 6.2504839 46 9 46 L 25.832031 46 A 1.0001 1.0001 0 0 0 26.158203 46 L 31.832031 46 A 1.0001 1.0001 0 0 0 32.158203 46 L 41 46 C 43.749516 46 46 43.749516 46 41 L 46 9 C 46 6.2504839 43.749516 4 41 4 L 9 4 z M 9 6 L 41 6 C 42.668484 6 44 7.3315161 44 9 L 44 41 C 44 42.668484 42.668484 44 41 44 L 33 44 L 33 30 L 36.820312 30 L 38.220703 23 L 33 23 L 33 21 C 33 20.442508 33.05305 20.398929 33.240234 20.277344 C 33.427419 20.155758 34.005822 20 35 20 L 38 20 L 38 14.369141 L 37.429688 14.097656 C 37.429688 14.097656 35.132647 13 32 13 C 29.75 13 27.901588 13.896453 26.71875 15.375 C 25.535912 16.853547 25 18.833333 25 21 L 25 23 L 22 23 L 22 30 L 25 30 L 25 44 L 9 44 C 7.3315161 44 6 42.668484 6 41 L 6 9 C 6 7.3315161 7.3315161 6 9 6 z M 32 15 C 34.079062 15 35.38736 15.458455 36 15.701172 L 36 18 L 35 18 C 33.849178 18 32.926956 18.0952 32.150391 18.599609 C 31.373826 19.104024 31 20.061492 31 21 L 31 25 L 35.779297 25 L 35.179688 28 L 31 28 L 31 44 L 27 44 L 27 28 L 24 28 L 24 25 L 27 25 L 27 21 C 27 19.166667 27.464088 17.646453 28.28125 16.625 C 29.098412 15.603547 30.25 15 32 15 z" /></svg>
                             </a>
@@ -58,11 +58,12 @@ function Company({ html, company }) {
                         <div className="">
                             <GoogleMaps latitude={location.lat} longitude={location.lng} />
                         </div>
+                        <div className="text-stone-50 justify-end flex pt-4">
+                            <button className="bg-blue-box hover:bg-button-blue text-white font-bold py-2 px-4 rounded-full" onClick={scrollToTop}>Scroll to Top</button>
+                        </div>
                     </div>
                 </div>
-                    <div className="text-stone-50 justify-end flex">
-                        <button className="bg-blue-box hover:bg-button-blue text-white font-bold py-2 px-4 rounded-full" onClick={scrollToTop}>Scroll to Top</button>
-                    </div>
+
             </div>
         </>
     )
